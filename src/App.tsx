e3958b8c80ae4032454c79f042510132457f4813
Marcus Escobar <marcusescobar10@gmail.com>
2025-02-28 21:12:21 -0300

    add path

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,6 @@ import './App.css'
 
 function App() {
 
-  const user = process.env.REACT_APP_API_USER;
-  const password = process.env.REACT_APP_API_PASSWORD;
-
-  console.log(`user: ${user}, password ${password}`);
   return (
     <>
       <h1>Hello World!!</h1>
